3637ca872877595e6326c3a4b9716086eafb629e
bykova_iuliia <lktisw@yandex.ru>
2021-05-08 22:54:49 +0300

    controll_component

--- src/components/Post/PostContent/CommentForm/CommentForm.tsx
+++ src/components/Post/PostContent/CommentForm/CommentForm.tsx
@@ -1,43 +1,39 @@
 import React, {
   ChangeEvent,
   FormEvent,
-  useContext,
   useEffect,
   useRef,
   useState,
 } from 'react';
-import { commentContext } from '../../../../context/commentContext';
 import s from './commentForm.less';
 import { CommentPanel } from './CommentPanel/CommentPanel';
 
 interface ICommentForm {
-  name : string;
+  name: string;
 }
 
-const CommentForm = ({name} : ICommentForm) => {
-  const { value, onChange } = useContext(commentContext);
+const CommentForm = ({ name }: ICommentForm) => {
+  const [value, onChange] = name
+    ? useState(name)
+    : useState('Оставьте ваш комментарий!');
+
   const myRef = useRef<HTMLTextAreaElement>(null);
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
   };
 
-  // контролируемый компонент
-  /*  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    onChange(event.target.value);
-    console.log(event.target.value);
-  };*/
-
- /*   useEffect(() => {
-    if (myRef.current) {
-      myRef.current?.focus();
-    }
-  });*/
-
-  // НЕконтролируемый компонент
-  const handleChange = () => {
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event?.target.value);
     console.log(myRef.current?.value);
   };
+
+  useEffect(() => {
+    if (myRef.current) {
+      myRef.current.focus();
+      onChange(name);
+    }
+  }, [name]);
 
   return (
     <form className={s.form} onSubmit={handleSubmit}>
@@ -48,9 +44,6 @@ const CommentForm = ({name} : ICommentForm) => {
           onChange={handleChange}
           value={value}
         />
-        <label className={s.label}>
-          <span>{name}</span> оставьте ваш комментарий
-        </label>
       </div>
 
       <div className={s.options}>
